718c11cb1366c633c625ab02080782e837e140aa
이경용 <sleekydz86@icloud.com>
2023-12-15 00:16:37 +0900

    액츄에이터 엔드포인트 정리
    액츄에이터 엔드포인트 정리

--- src/main/java/com/bootact/study/bootact/controller/BootActController.java
+++ src/main/java/com/bootact/study/bootact/controller/BootActController.java
@@ -1,4 +1,14 @@
 package com.bootact.study.bootact.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class BootActController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Everyone";
+    }
 }
--- src/main/java/com/bootact/study/bootact/custom/CustomEndPoint.java
+++ src/main/java/com/bootact/study/bootact/custom/CustomEndPoint.java
@@ -1,4 +1,14 @@
 package com.bootact.study.bootact.custom;
 
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+@Endpoint(id="customendpoint")
+@Component
 public class CustomEndPoint {
+    @ReadOperation
+    public String customInfo(){
+        return "this is a custom endpoint";
+    }
 }
